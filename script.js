e83eb5d428a908179b15bed53f6c5fb2eb7c06ed
Onno Paap <onno.paap@gmail.com>
2021-06-26 16:59:02 +0200

    html ready

---
+++ script.js
@@ -0,0 +1,12 @@
+import "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const queryParamsContainer = document.querySelector("[data-query-params]")
+const queryParamsContainer = document.querySelector("[data-query-params]")
+
+
+
+
+
+
+
